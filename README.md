df4f3f4744cf6e964582bacfc4f16605d5b2d8e8
AnwarKamel <anwarinfo24@gmail.com>
2020-09-08 10:00:33 +0100

    Upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Login-Screen 
+beautiful login screen|Android Studio Tutorial|Android Apps|Login Screen 
+
+## Screen
+
+![Screen1](capts/captr1.png)
+
+![Screen2](capts/captr3.png)
+
+![Demo](capts/cap2.gif) <br>
+
---
+++ app/src/main/java/com/ouail/anwarkamel/shopapp/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.ouail.anwarkamel.shopapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+
+    }
+}
